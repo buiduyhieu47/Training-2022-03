c8e8553b37c3ae4aab62bed65e424f5f305fda0b
buiduyhieu <buiduyhieu47@gmail.com>
2022-03-15 00:10:18 +0900

    Add file img and update code image link

--- js-practice/season/index.js
+++ js-practice/season/index.js
@@ -1,7 +1,7 @@
 // Click button 春 : to display image , title and change color to green for title 
 function spring() {
     document.getElementById("season").innerHTML = "春";
-    document.getElementById('image').src='/img/spring.jpeg';
+    document.getElementById('image').src='img/spring.jpeg';
     document.getElementById('title').style.color="green";
     
 }
@@ -9,20 +9,20 @@ function spring() {
 // Click button 夏 : to display image , title and change color to blue for title 
 function summer() {
     document.getElementById("season").innerHTML = "夏";
-    document.getElementById('image').src='summer.jpeg';
+    document.getElementById('image').src='img/summer.jpeg';
     document.getElementById('title').style.color="blue";
 }
 
 // Click button 秋 : to display image , title and change color to red for title
 function autumn() {
     document.getElementById("season").innerHTML = "秋";
-    document.getElementById('image').src='autumn.jpeg';
+    document.getElementById('image').src='img/autumn.jpeg';
     document.getElementById('title').style.color="red";
 }
 
 // Click button 冬 : to display image , title and change color to gray for title
 function winter() {
     document.getElementById("season").innerHTML = "冬";
-    document.getElementById('image').src='winter.jpeg';
+    document.getElementById('image').src='img/winter.jpeg';
     document.getElementById('title').style.color="gray";
 }
