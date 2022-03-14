40827c273b24be74d65b3e4c6a70b3c81be4de24
buiduyhieu <buiduyhieu47@gmail.com>
2022-03-14 23:55:33 +0900

    Fix code for change color of title

--- season/index.js
+++ season/index.js
@@ -1,27 +1,28 @@
+// Click button 春 : to display image , title and change color to green for title 
 function spring() {
-    
     document.getElementById("season").innerHTML = "春";
     document.getElementById('image').src='spring.jpeg';
-    document.getElementById('tittle').color=green;
+    document.getElementById('title').style.color="green";
+    
 }
 
+// Click button 夏 : to display image , title and change color to blue for title 
 function summer() {
-    
     document.getElementById("season").innerHTML = "夏";
     document.getElementById('image').src='summer.jpeg';
-    document.getElementById('tittle').style.color=blue;
+    document.getElementById('title').style.color="blue";
 }
 
+// Click button 秋 : to display image , title and change color to red for title
 function autumn() {
-    
     document.getElementById("season").innerHTML = "秋";
     document.getElementById('image').src='autumn.jpeg';
-    document.getElementById('tittle').style.color=yellow;
+    document.getElementById('title').style.color="red";
 }
 
+// Click button 冬 : to display image , title and change color to gray for title
 function winter() {
-    
     document.getElementById("season").innerHTML = "冬";
     document.getElementById('image').src='winter.jpeg';
-    document.getElementById('tittle').style.color=gray;
+    document.getElementById('title').style.color="gray";
 }
