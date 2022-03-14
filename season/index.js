c798b23a429ff37d52f2d2560c20910a56eb835f
buiduyhieu <buiduyhieu47@gmail.com>
2022-03-14 21:37:58 +0900

    Season by java script

---
+++ season/index.js
@@ -0,0 +1,27 @@
+function spring() {
+    
+    document.getElementById("season").innerHTML = "春";
+    document.getElementById('image').src='spring.jpeg';
+    document.getElementById('tittle').color=green;
+}
+
+function summer() {
+    
+    document.getElementById("season").innerHTML = "夏";
+    document.getElementById('image').src='summer.jpeg';
+    document.getElementById('tittle').style.color=blue;
+}
+
+function autumn() {
+    
+    document.getElementById("season").innerHTML = "秋";
+    document.getElementById('image').src='autumn.jpeg';
+    document.getElementById('tittle').style.color=yellow;
+}
+
+function winter() {
+    
+    document.getElementById("season").innerHTML = "冬";
+    document.getElementById('image').src='winter.jpeg';
+    document.getElementById('tittle').style.color=gray;
+}
